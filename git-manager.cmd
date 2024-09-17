a6d048427469e6c55c7fbd292a083c76c34079a3
konmil <milan.kondor06@gmail.com>
2024-09-17 10:39:09 +0200

    2024. 09. 17. - sok feladat

---
+++ git-manager.cmd
@@ -0,0 +1,30 @@
+@echo off
+pushd %~0\..
+:choice
+set /p choice="Push(1) vagy pull(2)? > "
+if %choice% == 1 (
+    goto :push
+)
+if %choice% == 2 (
+    goto :pull
+) else (
+    echo Hibás válasz
+    timeout 2 /nobreak >nul
+    goto :choice
+)
+
+:push
+git add .
+set /p commit="Commit message > "
+git commit -m "%date% - %commit%"
+git push
+goto :end
+
+:pull
+git pull
+goto :end
+
+:end
+net helpmsg %errorlevel%
+timeout 5 /nobreak >nul
+exit /b %errorlevel%
